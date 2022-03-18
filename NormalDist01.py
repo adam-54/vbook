af5707fc38f768914a34f192525503b42e8ec081
adam-54 <80105679+adam-54@users.noreply.github.com>
2022-03-18 17:33:41 +0100

    Add files via upload

--- NormalDist01.py
+++ NormalDist01.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 #Linspace - evenly spaced number. 
 #Norm.Ppf critical values from standard normal distribution
 x = np.linspace(norm.ppf(0.001),norm.ppf(0.999), 100)
-f_x = norm.pdf(x) #pdf
-#f_x = norm.cdf(x) #cdf
+f_x = norm.pdf(x,0,1) #pdf
+#f_x = norm.cdf(x,0,1) #cdf
 
 #Figure
 fig = plt.figure()
@@ -14,9 +14,11 @@ ax = plt.axes()
 ax.plot(x, f_x)
 
 
-#Random number:
-np.random.seed(10)
-rand = norm.rvs(size=1000)
+#Random sample of size n
+np.random.seed(2022)
+
+n = 1000
+rand = norm.rvs(size=n)
 
 #Hitogram, alpha - transparency, rwidth - bar width
 n, bins, patches = plt.hist(x=rand, bins='auto', color='#0504aa',alpha=0.7, rwidth=0.9)
