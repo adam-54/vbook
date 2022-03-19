c36e21fc7994fa920c8c436af0558692c4620e70
adam-54 <80105679+adam-54@users.noreply.github.com>
2022-03-19 13:13:28 +0100

    Add files via upload

--- BetaDistShiny01.R
+++ BetaDistShiny01.R
@@ -10,23 +10,23 @@ library(scales)
 ui <-shinyUI(fluidPage(
   
   #Name the Title of the applicationh
-  titlePanel("Normal distribution"),
+  titlePanel("Beta distribution"),
   
   #Create a sidebar with a slider input and selection input
   sidebarLayout(
     sidebarPanel(
       numericInput("lrange", 
                    "Lower bound of the range", 
-                   value = -3.5),
+                   value = 0),
       numericInput("urange", 
                    "Upper bound of the range", 
-                   value = 3.5),
-      numericInput("mean", 
-                   "Mean", 
-                   value = 0),
-      numericInput("std", 
-                   "Standard deviation", 
                    value = 1),
+      numericInput("a", 
+                   "a", 
+                   value = 2),
+      numericInput("b", 
+                   "b", 
+                   value = 5),
       sliderInput("n","Sample size", min = 0,max = 10000,value = 100),
     ),
     
@@ -45,7 +45,7 @@ server <- shinyServer(function(input, output) {
   
   #Derive element common for several outputs - arguments for PDF and CDF
   x_var <- reactive({
-    seq(input$lrange, input$urange, by = 0.1)
+    seq(input$lrange, input$urange, by = 0.01)
   })
   
   #2.
@@ -57,10 +57,10 @@ server <- shinyServer(function(input, output) {
     x<-x_var()
     
     #Find the PDF function values for selected range of arguments
-    y <- dnorm(x, mean = input$mean, sd = input$std)
+    y <- dbeta(x, shape1 = input$a, shape2 = input$b)
     
     #Draw the plot
-    plot(x,y, type="l",main='Probability density function')
+    plot(x,y,type="l",main='Probability density function')
   })
   
   #CDF plot
@@ -70,10 +70,10 @@ server <- shinyServer(function(input, output) {
     x<-x_var()
     
     #Find the PDF function values for selected range of arguments
-    y <- pnorm(x, mean = input$mean, sd = input$std)
+    y <- pbeta(x, shape1 = input$a, shape2 = input$b)
     
     #Draw the plot
-    plot(x,y, type="l",main='Cumulative distribution function')
+    plot(x,y,type="l",main='Cumulative distribution function')
   })
   
   
@@ -82,7 +82,7 @@ server <- shinyServer(function(input, output) {
     
     #Random draws
     #set.seed(5)
-    x <- rnorm(input$n,mean = input$mean, sd = input$std)
+    x <- rbeta(input$n,shape1 = input$a, shape2 = input$b)
     
     #Draw the histogram
     hist(x, main = "Histogram of sampled data")
